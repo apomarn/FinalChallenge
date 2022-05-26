6cda8b6a45f575087e0eef87753a03e4377bc503
Anghy Pomar <anghypomar@Anghys-MacBook-Pro.local>
2022-05-25 23:35:09 -0400

    peliculas

---
+++ peliculas.js
@@ -0,0 +1,78 @@
+let peliculas =[
+    {
+        id: 1,
+        nombre: "Alerta Roja",
+        duracionHoras: 2,
+        categoria: "Accion",
+        año: 2021,
+        rate: 1
+    },
+    {
+        id: 2,
+        nombre: "Blade Runner 2049",
+        duracionHoras: 2,
+        categoria: "Accion",
+        año: 2017, 
+        rate: 2
+    },
+    {
+        id: 3,
+        nombre: "El anio de mi graduacion",
+        duracionHoras: 2,
+        categoria: "Comedia",
+        año: 2022, 
+        rate: 3
+    },
+    {
+        id: 4,
+        nombre: "La otra Missy",
+        duracionHoras: 2,
+        categoria: "Comedia",
+        año: 2020, 
+        rate: 4
+    },
+    {
+        id: 5,
+        nombre: "El misterio de Marilyn Monroe",
+        duracionHoras: 1,
+        categoria: "Documental",
+        año: 2022, 
+        rate: 5
+    },
+    {
+        id: 6,
+        nombre: "Los secretos de la Tumba de Saqqara",
+        duracionHoras: 2,
+        categoria: "Documental",
+        año: 2020, 
+        rate: 4
+    },
+    {
+        id: 7,
+        nombre: "El Proyecto Adam",
+        duracionHoras: 1,
+        categoria: "Accion",
+        año: 2022, 
+        rate: 3
+    },
+    {
+        id: 8,
+        nombre: "No miren arriba",
+        duracionHoras: 3,
+        categoria: "Accion",
+        año: 2021, 
+        rate: 5
+    },
+    {
+        id: 9,
+        nombre: "Bird Box: A ciegas",
+        duracionHoras: 3,
+        categoria: "Accion",
+        año: 2018, 
+        rate: 3
+    },
+
+
+    
+        
+]
