a5d7d4cefe268031001c5d6f8d08e4354c4a1221
Anghy Pomar <anghypomar@Anghys-MacBook-Pro.local>
2022-06-14 20:17:09 -0400

    forms included

--- codigo.js
+++ codigo.js
@@ -11,6 +11,7 @@ class Pelicula {
     constructor(pelicula){
         this.id=pelicula.id;
         this.nombre=pelicula.nombre;
+        this.image=pelicula.image;
         this.duracionHoras=pelicula.duracionHoras;
         this.categoria=pelicula.categoria;
         this.año=pelicula.año;
@@ -18,43 +19,166 @@ class Pelicula {
     }
 }
 
+//form agregar
+
+
+function eraseAddForm() {
+    const agregarPelicula = document.getElementById("agregarPelicula");
+    agregarPelicula.innerHTML = ``
+}
+
+function eraseRemoveForm(){
+    const removePelicula = document.getElementById("removerPelicula");
+    removePelicula.innerHTML = ``
+}
+
+function hideButtons() {
+    const agregarRemover = document.getElementById("agregarRemover");
+    agregarRemover.style.display = "none";
+}
+function showButtons() {
+    const agregarRemover = document.getElementById("agregarRemover");
+    agregarRemover.style.display = "block";
+}
+
+
+
 class Producer {
     constructor(peliculas) {
         this.peliculas = peliculas
     }
-
+    
     add(){
-        let id = prompt("Agrega el numero de id para tu pelicula")
-        let nombre=prompt("Agrega el nuevo nombre de una pelicula");
-        let duracionHoras=prompt("Cuanto dura la pelicula?");
-        let categoria=prompt("Que categoria es esa pelicula?");
-        let año=prompt("De que año es esa pelicula?");
-        let rate=prompt("Que rate tiene la pelicula? (Escribe del 1 al 5)")
+        
+        this.showForm();
+        hideButtons();
+    
+            
+    }
+    showForm() {
 
-        this.peliculas.push(new Pelicula({id, nombre, duracionHoras, categoria, año, rate}))
+        const agregarPelicula = document.getElementById("agregarPelicula")
+                agregarPelicula.innerHTML = `
+                <form >
+                <label name="id">Ingresa el numero ID de tu pelicula</label><br>
+                <input type="number" id="id" name="id"></input><br>
+        
+                <label name="nombre">Ingresa el nombre de tu pelicula</label><br>
+                <input type="text" id="nombre" name="nombre"></input><br>
+        
+                <label name="image">Ingresa el link de la imagen de tu pelicula</label><br>
+                <input type="text" id="image" name="image"></input><br>
+        
+                <label name="duracionHoras">Ingresa la duracion de tu pelicula en horas</label><br>
+                <input type="number" id="duracionHoras" name="duracionHoras"></input><br>
+        
+                <label name="categoria">Ingresa la categoria de tu pelicula</label><br>
+                <input type="checkbox" class="categorias" value="accion" name="accion">Accion</input><br>
+                <input type="checkbox" class="categorias" value="comedia" name="comedia">Comedia</input><br>
+                <input type="checkbox" class="categorias" value="documental" name="documental">Documental</input><br>
+                <input type="checkbox" class="categorias" value="drama" name="drama">Drama</input><br>
+                <input type="checkbox" class="categorias" value="infantil" name="infantil">Infantil</input><br>
+                <input type="checkbox" class="categorias" value="terror" name="terror">Terror</input><br>
+        
+                <label name="año">Ingresa el año de tu pelicula</label><br>
+                <input type="number" id="año" name="año"></input><br>
+        
+                <label name="rate">Ingresa el rate de tu pelicula</label><br>
+                <input class="rates" type="checkbox" value="rate1" name="rate1">1</input><br>
+                <input class="rates" type="checkbox" value="rate2" name="rate2">2</input><br>
+                <input class="rates" type="checkbox" value="rate3" name="rate3">3</input><br>
+                <input class="rates" type="checkbox" value="rate4" name="rate4">4</input><br>
+                <input class="rates" type="checkbox" value="rate5" name="rate5">5</input><br>
+
+                <input class="boton" type="button" value="Agregar Pelicula" onclick="Netflix.onSubmitAdd()"></input>
+
+            </form>
+                `
+            
+    
+    }
+    
+    inputChecked (array) {
+        console.log(array)
+        let peliculaChecked = [];
+        for (const isItChecked of array) {
+            if(isItChecked.checked === true) {
+                peliculaChecked.push(isItChecked.value);
+                }
+            }
+
+            return peliculaChecked;
+    }
+    onSubmitAdd(){
+
+        console.log("onsubmit")
+      
+    
+        let id = document.getElementById("id").value;
+                    let nombre= document.getElementById("nombre").value;
+                    let image=document.getElementById("image").value;
+                    let duracionHoras=document.getElementById("duracionHoras").value;
+                    let categorias=  document.getElementsByClassName("categorias");
+                    let categoria = this.inputChecked(categorias);
+                    let año=document.getElementById("duracionHoras").value;
+                    let rates= document.getElementsByClassName("rates");
+                    let rate=this.inputChecked(rates);
+            
+                
+                    console.log({id, nombre, image, duracionHoras, categoria, año, rate})
+            
+                this.peliculas.push( new Pelicula({id, nombre, image, duracionHoras, categoria, año, rate}))
+                updatingPeliculas(this.peliculas);
+                this.movieQuantity();
+                eraseAddForm();
+                showButtons();
     }
     
     remove(){
-        let toRemove = parseInt(prompt("Ingresa el ID de la pelicula que deseas eliminar"));
 
-       for(const unaPelicula of peliculas){
-           console.log(unaPelicula.id)
-           if(unaPelicula.id == null) {
-               prompt("Porfavor vuelva a ingresar el numero ID de la pelicula que deseas eliminar")
-           } else {
-               const index = peliculas.findIndex(function (pelicula) {
-                return pelicula.id === toRemove
-            })
+        hideButtons();
 
-           return  this.peliculas.splice(index, 1);
-
-            }
-
-        }   
+        let checkboxes = ''
+        for(const pelicula of this.peliculas) {
+            checkboxes += `<input type="checkbox" class="listaRemover" value="${pelicula.nombre}">${pelicula.nombre}</input>`
+        }
+        console.log(checkboxes)
+        let toRemove = document.getElementById("removerPelicula");
+        toRemove.innerHTML = `
+        <form>
+            <p>Cual de las peliculas deseas borrar?</p>
+            ${checkboxes}
+            <input class="boton" type="button" value="Borrar Pelicula" onclick="Netflix.onSubmitRemove()"></input>
+        </form>
+    `
+   
     }
 
+    onSubmitRemove () {
+        let removeArray = document.getElementsByClassName("listaRemover");
+        let removeChecked = this.inputChecked(removeArray);
+
+        let leftPeliculas = []
+
+        for (const pelicula of this.peliculas) {
+            if(removeChecked.includes(pelicula.nombre) === false) {
+                leftPeliculas.push(pelicula)
+            }
+        }
+
+        this.peliculas = leftPeliculas
+
+ 
+         updatingPeliculas(this.peliculas);
+         this.movieQuantity();
+         eraseRemoveForm();
+         showButtons();
+           
+    }
+ 
     orderPerYear(){
-        return this.peliculas.sort((a, b) => {
+       
+        let sorted = this.peliculas.sort((a, b) => {
             const añoA = a.año;
             const añoB = b.año;
     
@@ -68,28 +192,52 @@ class Producer {
 
             return comparison
         });
+       updatingPeliculas(sorted);
     }
 
     recommended(){
 
         let recomendados = []
         for (const pelicula of this.peliculas) {
-            if(pelicula.rate === 4 || pelicula.rate ===5) {
+            if(pelicula.rate === 4 || pelicula.rate === 5) {
                 recomendados.push(pelicula)
             }
         }
 
-        return recomendados
+        let carousel = document.getElementsByClassName("cadaRecomendado")[0]
+
+        for (const unRecomendado of recomendados) {
+            carousel.innerHTML += `
+            <div class="carousel-item active" data-bs-interval="2000" >
+                <img
+                src="${unRecomendado.image}"
+                class="d-block w-100"
+                alt="${unRecomendado.nombre}"
+                />
+                <p style="color: white;">${unRecomendado.nombre}</p>
+            </div>
+            
+            `
+        }
+       
+
+        
     }
+    
 
     movieQuantity(){
-        let quantity = this.peliculas.length;
-        console.log("El total de la lista de peliculas es " + quantity)
-        return console.table(this.peliculas);
+    let quantity = document.getElementById("movieQuantity");
+    quantity.style.background="pink";
+    quantity.style.height="100px";
+    quantity.style.width="100px";
+    quantity.style.color="black";
+    return quantity.innerHTML = `
+
+        <p> Total de Peliculas: ${this.peliculas.length}</p>
+        `
 
     }
 
-    
 }
 
 
@@ -128,7 +276,16 @@ for (const boton of botones) {
 }
 
 
+
+
+
+
+
+
 //div de peliculas
+
+
+
 let listaPeliculas = document.createElement("div");
 listaPeliculas.style.display="flex";
 listaPeliculas.style.height="75px";
@@ -137,45 +294,79 @@ listaPeliculas.style.alignItems="space between";
 listaPeliculas.style.flexWrap="wrap"
 
 
-
-
-//cada pelicula
-for(const pelicula of Netflix.peliculas) {
-    let contenedorPelicula = document.createElement("div");
-    
-
-    contenedorPelicula.innerHTML = `
-       <img class="peliculasImage" src="https://thumbs.dreamstime.com/b/big-open-clapper-board-movie-reel-cinema-icon-set-movie-film-elements-flat-design-cinema-movie-time-flat-icons-f-95500226.jpg">;
-       <p class="peliculasNombre">${pelicula.nombre}</p>
-    `
-   contenedorPelicula.style.margin="30px";
-    listaPeliculas.append(contenedorPelicula)
+function borrarTodasPeliculas (parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
-let peliculasImage = document.getElementsByClassName("peliculasImage");
-setTimeout(function() {
-    for (const image of peliculasImage){
-        image.style.height= "150px";
-        image.style.width="150px";
+
+updatingPeliculas(Netflix.peliculas);
+Netflix.movieQuantity()
+Netflix.orderPerYear()
+Netflix.recommended()
+
+
+//container con todas las peliculas
+
+function updatingPeliculas(movies){
+    const listaPeliculass = document.getElementById("listaPeliculas");
+    listaPeliculass.style.display="flex";
+    listaPeliculass.style.height="75px";
+    listaPeliculass.style.justifyContent="center";
+    listaPeliculass.style.alignItems="space between";
+    listaPeliculass.style.flexWrap="wrap"
+
+    borrarTodasPeliculas(listaPeliculass);
+
+    for(const pelicula of movies) {
+
+        let contenedorPelicula = document.createElement("div");
+        
+    
+        contenedorPelicula.innerHTML = `
+           <img class="peliculasImage" src="${pelicula.image}">;
+           <p class="peliculasNombre">${pelicula.nombre}</p>
+        `
+       contenedorPelicula.style.margin="30px";
+        listaPeliculass.append(contenedorPelicula)
     }
-},0)
-
-let peliculasNombre = document.getElementsByClassName("peliculasNombre");
-setTimeout(function() {
-    for (const nombre of peliculasNombre){
-        nombre.style.height= "50px";
-        nombre.style.width="150px";
-        nombre.style.color="white";
-        nombre.style.fontSize="16px";
-      
-    }
-},0)
 
 
+
+    
+    let peliculasImage = document.getElementsByClassName("peliculasImage");
+    setTimeout(function() {
+        for (const image of peliculasImage){
+            image.style.height= "150px";
+            image.style.width="150px";
+        }
+    },0)
+    
+    let peliculasNombre = document.getElementsByClassName("peliculasNombre");
+    setTimeout(function() {
+        for (const nombre of peliculasNombre){
+            nombre.style.height= "50px";
+            nombre.style.width="150px";
+            nombre.style.color="white";
+            nombre.style.fontSize="16px";
+          
+        }
+    },0)
+
+}
 
 
 let contenedorLista = document.getElementById("listaPeliculas");
 contenedorLista.append(listaPeliculas)
+
+
+
+
+
+
+
+
 
 
 /****
--- peliculas.js
+++ peliculas.js
@@ -2,6 +2,7 @@ let peliculas =[
     {
         id: 1,
         nombre: "Alerta Roja",
+        image: "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2021/11/15/16369737576486.jpg",
         duracionHoras: 2,
         categoria: "Accion",
         año: 2021,
@@ -10,6 +11,7 @@ let peliculas =[
     {
         id: 2,
         nombre: "Blade Runner 2049",
+        image:"https://cdn3.whatculture.com/images/2021/01/0b0df33652d75ef7-1200x675.jpg",
         duracionHoras: 2,
         categoria: "Accion",
         año: 2017, 
@@ -18,6 +20,7 @@ let peliculas =[
     {
         id: 3,
         nombre: "El anio de mi graduacion",
+        image: "https://hiramnoriega.com/wp-content/uploads/2022/05/32434654869679789879.jpg",
         duracionHoras: 2,
         categoria: "Comedia",
         año: 2022, 
@@ -26,6 +29,7 @@ let peliculas =[
     {
         id: 4,
         nombre: "La otra Missy",
+        image: "https://ca-times.brightspotcdn.com/dims4/default/fbf6651/2147483647/strip/true/crop/7200x4800+0+0/resize/1486x991!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fdf%2Fa4%2F092bb9b1492c9826b98f2ca15684%2Fla-photos-handouts-la-et-the-wrong-missy-movie-136.JPG",
         duracionHoras: 2,
         categoria: "Comedia",
         año: 2020, 
@@ -34,6 +38,7 @@ let peliculas =[
     {
         id: 5,
         nombre: "El misterio de Marilyn Monroe",
+        image: "https://www.infobae.com/new-resizer/PdgFRf1YdjnJBWMxBgkfN9x6blM=/1200x900/filters:format(webp):quality(85)//cloudfront-us-east-1.images.arcpublishing.com/infobae/HXAZL7NDDZBAXPDK3Q3BF4XYDE.jpg",
         duracionHoras: 1,
         categoria: "Documental",
         año: 2022, 
@@ -42,6 +47,7 @@ let peliculas =[
     {
         id: 6,
         nombre: "Los secretos de la Tumba de Saqqara",
+        image: "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABfy7o8ZE7_3nrzc6z80udsL8AmyGVBhJ7iyvNXmUQoikgUt-QXVh_smuuvQSzcOJPAZfmjTWiUGVdAdXjq8f25TnDQCnXk8_mFuu.jpg?r=9b6",
         duracionHoras: 2,
         categoria: "Documental",
         año: 2020, 
@@ -50,6 +56,7 @@ let peliculas =[
     {
         id: 7,
         nombre: "El Proyecto Adam",
+        image: "https://aiptcomics.com/wp-content/uploads/2022/03/the-adam-project-1-1.jpg",
         duracionHoras: 1,
         categoria: "Accion",
         año: 2022, 
@@ -58,17 +65,19 @@ let peliculas =[
     {
         id: 8,
         nombre: "No miren arriba",
+        image: "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABf2NpNhZsRSLwu3n0KhysCBpHNRlTQu86BLXuxSsleAMrjLNF6G0COGO2-s8YUywD64vHPiX5knGmTkMgF6A002TGi6eilpE329N.jpg?r=7c6",
         duracionHoras: 3,
         categoria: "Accion",
         año: 2021, 
         rate: 5
     },
     {
-        id: 9,
+        id: 19,
         nombre: "Bird Box: A ciegas",
+        image: "https://static.onecms.io/wp-content/uploads/sites/6/2019/03/bird_box_018-2000.jpg",
         duracionHoras: 3,
         categoria: "Accion",
-        año: 2018, 
+        año: 10, 
         rate: 3
     },
 
